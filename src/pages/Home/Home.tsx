b79ab20b74751f0a4b04c7cd9dcfdc85c7439ac6
isalim079 <salimreza1025@gmail.com>
2024-09-09 20:20:13 +0600

    scroll to top button  added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import FeaturedFacilities from "./FeaturedFacilities/FeaturedFacilities";
 import HowItWorks from "./HowItWorks/HowItWorks";
 import TopBanner from "./TopBanner/TopBanner";
 import UpcomingEvents from "./UpcomingEvents/UpcomingEvents";
+import ScrollToTop from "react-scroll-to-top";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <HowItWorks />
       <CustomerTestimonials />
       <UpcomingEvents />
+      <ScrollToTop className="pl-[14px] pr-[40px]" smooth />
     </div>
   );
 };
