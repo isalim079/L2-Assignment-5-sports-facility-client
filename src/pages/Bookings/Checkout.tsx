09e56d944396c50a54557a33c88215dd1144b9e9
isalim079 <salimreza1025@gmail.com>
2024-09-10 23:13:56 +0600

    payment problem solved

--- src/pages/Bookings/Checkout.tsx
+++ src/pages/Bookings/Checkout.tsx
@@ -3,7 +3,7 @@ import { Elements } from "@stripe/react-stripe-js";
 
 import CheckOutForm from "./CheckOutForm";
 
-const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
+const stripePromise = loadStripe(`${import.meta.env.VITE_Payment_Gateway_PK}`);
 // console.log(stripePromise);
 
 const Checkout = () => {
